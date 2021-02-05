9c4cb9304ed15533a6034b34a07d210b8ac9a701
KarthigaVijaykumar <karthigamadhu8599@gmail.com>
2021-02-06 00:07:01 +0530

    created feature branch and made some changes

--- git-practice/src/practice/Program1.java
+++ git-practice/src/practice/Program1.java
@@ -2,7 +2,7 @@ package practice;
 
 public class Program1 {
 	public static void main(String[] args) {
-		String fruits[]  = {"apple","mango","lemon","papaya"};
+		String fruits[]  = {"apple","mango","lemon","papaya","orange,lemon"};
 		for(String fruit : fruits) {
 			System.out.println(fruit);
 		}
