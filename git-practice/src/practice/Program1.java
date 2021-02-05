2491645685cac863c3f9ab790b2505680126c7e3
KarthigaVijaykumar <karthigamadhu8599@gmail.com>
2021-02-06 00:01:26 +0530

    Created a file in master

---
+++ git-practice/src/practice/Program1.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Program1 {
+	public static void main(String[] args) {
+		String fruits[]  = {"apple","mango","lemon","papaya"};
+		for(String fruit : fruits) {
+			System.out.println(fruit);
+		}
+	}
+}
